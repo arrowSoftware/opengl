00439a71e1169f4c92f4043ce9afb422130ef41b
arrowSoftware <tyler.gajewski01@gmail.com>
2019-07-29 23:05:11 -0400

    renaming Bootstrap to bootstrap

--- tyler.sh
+++ tyler.sh
@@ -16,7 +16,7 @@ do
         build=debug
     elif [ "$arg" == "lint" ]
     then
-        cpplint --filter=-legal/copyright,-whitespace/braces,-whitespace/comments,-whitespace/indent "$@"
+        cpplint --recursive --filter=-legal/copyright,-whitespace/braces,-whitespace/comments,-whitespace/indent,-build/include_order "${@:2}"
         exit
     fi
 done
